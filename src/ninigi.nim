28f576c9cee8f1f7d6fde1fe5f89f1fca46d9e42
Kazuya Takei <myself@attakei.net>
2021-11-30 01:01:11 +0900

    Move main into submodule

--- src/ninigi.nim
+++ src/ninigi.nim
@@ -1,37 +1,8 @@
-import std/[httpclient, json]
-import ninigipkg/[core, parser]
+import ninigipkg/core
 
-
-const DEFAULT_API_URL = "https://www.toptal.com/developers/gitignore/api/list?format=json"
-
-
-proc newTemplatesFromWeb*(url: string = DEFAULT_API_URL): TemplatesTable =
-  let data = newHttpClient().get(url).body.parseJson()
-  result = parseTemplates(data)
+export core
 
 
 when isMainModule:
-  import std/[os, parseopt, tables]
-  var p = initOptParser(commandLineParams())
-  var targets: seq[string] = @[]
-  while true:
-    p.next()
-    case p.kind:
-      of cmdEnd: break
-      of cmdLongOption, cmdShortOption:
-        discard
-      of cmdArgument:
-        targets.add(p.key)
-
-  if targets.len == 0:
-    stderr.writeLine("No targets is specified.")
-    quit(1)
-
-  let templates = newTemplatesFromWeb()
-  stdout.writeLine("### NINIGI version 0.1.0")
-  for t in targets:
-    if not templates.hasKey(t):
-      stdout.writeLine("## '" & t & "' is not exists")
-      continue
-    stdout.writeLine("")
-    stdout.writeLine(templates[t].output)
+  import ninigipkg/cli
+  quit(main())
---
+++ src/ninigipkg/cli.nim
@@ -0,0 +1,43 @@
+import std/[httpclient, json, os, parseopt, tables]
+import core
+import parser
+
+
+const DEFAULT_API_URL = "https://www.toptal.com/developers/gitignore/api/list?format=json"
+
+
+proc newTemplatesFromWeb*(url: string = DEFAULT_API_URL): TemplatesTable =
+  let data = newHttpClient().get(url).body.parseJson()
+  result = parseTemplates(data)
+
+
+proc parseArgs(params: seq[string]): seq[string] =
+  result = @[]
+  var p = initOptParser(params)
+  while true:
+    p.next()
+    case p.kind:
+      of cmdEnd: break
+      of cmdLongOption, cmdShortOption:
+        discard
+      of cmdArgument:
+        result.add(p.key)
+
+
+proc main*(): int =
+  let targets = parseArgs(commandLineParams())
+
+  if targets.len == 0:
+    stderr.writeLine("No targets is specified.")
+    return 1
+
+  let templates = newTemplatesFromWeb()
+  stdout.writeLine("### NINIGI version 0.1.0")
+  for t in targets:
+    if not templates.hasKey(t):
+      stdout.writeLine("## '" & t & "' is not exists")
+      continue
+    stdout.writeLine("")
+    stdout.writeLine(templates[t].output)
+
+  return 0
